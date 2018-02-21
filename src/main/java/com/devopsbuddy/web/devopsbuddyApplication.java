b8c5732845cb74bab056d81f11d628a16581e89d
sakshi <sakshikansal08@gmail.com>
2018-02-21 14:25:58 +1100

    created index file

--- src/main/java/com/devopsbuddy/web/devopsbuddyApplication.java
+++ src/main/java/com/devopsbuddy/web/devopsbuddyApplication.java
@@ -1,12 +1,12 @@
-package com.example.demo;
+package com.devopsbuddy.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo1Application {
+public class devopsbuddyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demo1Application.class, args);
+		SpringApplication.run(devopsbuddyApplication.class, args);
 	}
 }
